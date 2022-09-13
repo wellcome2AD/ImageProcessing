325cff008d9c319057b998a294d7596c95d77622
Korobeynikova Alice <iuuyyttttt@mail.ru>
2022-09-13 12:58:02 +0300

    Delete unnecassary usings

--- Dilation.cs
+++ Dilation.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace ImageProcessing
 {
-    class Dilation //: Filters
+    class Dilation
     {
         bool[,] mask;
         void createMask()
