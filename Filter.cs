400c8c43c4758e2cf95b434f44c51fe3c98364a9
Korobeynikova Alice <iuuyyttttt@mail.ru>
2022-09-13 12:55:42 +0300

    Filters was renamed to Filter

--- Filter.cs
+++ Filter.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
+﻿using System.Drawing;
 using System.ComponentModel;
 
 namespace ImageProcessing
 {
-    abstract class Filters
+    abstract class Filter
     {
         public virtual Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
         {
